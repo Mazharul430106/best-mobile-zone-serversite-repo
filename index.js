078f3716ad26c98e50f9f6f264d317bf336de6c3
Mazharul Islam <mazharul430@gmail.com>
2022-11-24 11:15:20 -0800

    get category section data

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qm6ghoc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run =  async ()=>{
+    try{
+        const categoryCollection = client.db('bestMobileZone').collection('mobileCategories');
+
+        app.get('/categories', async(req, res)=>{
+            const query = {};
+            const categories = await categoryCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(error=> {
+    console.log(error);
+})
+
 
 
 
